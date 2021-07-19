8d67717e790a72d56e783c1f6fa16df473244a78
Justin Philpott <justin.philpott@protonmail.com>
2021-07-20 00:19:11 +0100

    Test CSS new variable

--- components/screens/ScreenStart.tsx
+++ components/screens/ScreenStart.tsx
@@ -17,7 +17,7 @@ export const ScreenStart = () => {
 
   return (
     <>
-      <Flex direction="column" justifyContent="space-between" height="100vh" p="12" rounded="6" position="relative">
+      <Flex direction="column" justifyContent="space-between" height="calc(var(--vh, 1vh) * 100)" p="12" rounded="6" position="relative">
         <Heading as="h1" size="4xl" color="#FDC173" mb={6} textShadow="0 0 0.4em #0A3839">The Tower of Hanoi</Heading>
         <Flex direction="row" flexWrap="wrap" background="rgba(255, 255, 255, 0.9)" p="6" pb={0} rounded="6">
         <Button size="md" textShadow="0px 0px 10px #fff" flexGrow={1} flexBasis={0} minWidth="100px" colorScheme="teal" color="#000" m="0 0.5em 1.5em 0.5em" onClick={ () => onPlay() }>Play</Button>
