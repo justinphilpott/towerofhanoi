d18fe33b3e4bdaf64d4753633a1f1f0bfe1e8279
Justin Philpott <justin.philpott@protonmail.com>
2021-07-28 22:41:38 +0100

    rename

--- components/screens/ScreenStart.tsx
+++ components/screens/ScreenStart.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, {useLayoutEffect} from 'react'
 import {
   Button,
   Heading,
   Flex,
   Text
 } from "@chakra-ui/react"
-import  { useScreenSend } from './fsm/ScreenFSMProvider';
+import { useScreenSend } from './fsm/ScreenFSMProvider';
+import { useScreenAspect } from '../TowerofHanoi/utils/useScreenAspect';
 
 export const ScreenStart = () => {
 
   const send = useScreenSend();
+  const aspect = useScreenAspect();
 
   return (
     <>
-      <Flex direction="column" justifyContent="space-between" height="calc(var(--vh, 1vh) * 100)" p="12" rounded="6" position="relative">
-        <Heading as="h1" size="4xl" color="#FDC173" mb={6} textShadow="0 0 0.4em #0A3839">The Tower of Hanoi</Heading>
+      <Flex direction="column" justifyContent="space-between" height="calc(var(--vh, 1vh) * 100)" p={{ base: 6, sm: 8, md: 12, lg: 16, xl: 24 }} rounded="6" position="relative">
+        <Heading as="h1" size="4xl" color="#FDC173" mb={6} mt={3} textShadow="0 0 0.4em #0A3839">The Tower of Hanoi</Heading>
         <Flex direction="column" background="rgba(255, 255, 255, 0.9)" p={3} pb={0} rounded={6}>
           <Text fontSize="lg" color="#000" m={3} mb={6} textAlign="center" fontWeight="bold">Train your brain with this famous puzzle!</Text>
+          {aspect < 1 &&
+            <>
+               rotate
+            </>
+          }
           <Flex direction="row" flexWrap="wrap">
             <Button size="md" textShadow="0px 0px 10px #fff" flexGrow={1} flexBasis={0} minWidth="100px" colorScheme="teal" color="#000" m="0 0.5em 1.5em 0.5em" onClick={ () => send({ type: 'PLAY' }) }>Play</Button>
             <Button size="md" textShadow="0px 0px 10px #fff" flexGrow={1} flexBasis={0} minWidth="100px" colorScheme="gold" color="#000" m="0 0.5em 1.5em 0.5em" onClick={ () => send({ type: 'TUTORIAL' }) }>How to play</Button>
---
+++ components/towerofhanoi/utils/useScreenAspect.ts
@@ -0,0 +1,56 @@
+import { useState, useLayoutEffect } from 'react'
+
+declare global {
+  interface Window {
+    availHeight?: number;
+    availWidth?: number;
+  }
+}
+
+/**
+ * Gives the aspect ratio of the screen
+ *
+ * Depending on circumstances this may or may not allow
+ * one to infer orientation e.g. an onscreen keyboard
+ *
+ * @returns Float - screen aspect ratio
+ *
+ * @see https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
+ * @see https://css-tricks.com/debouncing-throttling-explained-examples/
+ * @see https://www.typescriptlang.org/docs/handbook/2/functions.html#declaring-this-in-a-function
+ */
+export const useScreenAspect = () => {
+
+  const calcAspect = ():number => {
+    return window.screen.availWidth / window.screen.availHeight;
+  }
+
+  const [aspect, setAspect] = useState(calcAspect());
+  const [timeoutState, setTimeoutState] = useState(false);
+
+  // debounce function
+  const db = (fn: () => void, milliseconds: number) => {
+    let timer: number | null;
+    return function() {
+      clearTimeout(timer as number);
+        timer = window.setTimeout(function(this: () => {}){
+        timer = null;
+        fn.apply(this);
+      }, milliseconds)
+    };
+  }
+
+  // capture the resize and get update the aspect
+  useLayoutEffect(() => {
+    const db_handleResize = db(function handleResize() {
+      console.log('handle resize');
+      setAspect(calcAspect());
+    }, 500);
+    window.addEventListener('resize', db_handleResize);
+    return () => {
+      window.removeEventListener('resize', db_handleResize);
+    };
+  });
+
+  return aspect;
+}
