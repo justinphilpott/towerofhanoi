3993c71a87d0bbd061a4f95653bda8634a105912
Justin Philpott <justin.philpott@protonmail.com>
2021-08-09 22:15:47 +0100

    Font subsetting done

--- theme-fonts.js
+++ theme-fonts.js
@@ -8,7 +8,8 @@ const Fonts = () => (
         font-style: normal;
         font-display: swap;
         font-weight: 400;
-        src: url('./fonts/pattaya-latin-400-normal.woff2') format('woff2');
+        src: url('./fonts/pattaya-regular-webfont.woff2') format('woff2'), url('./fonts/pattaya-regular-webfont.woff') format('woff');
+        unicode-range: U+21,U+2E,U+3F,U+47,U+48,U+4C,U+51,U+53,U+54,U+57,U+61,U+63-69,U+6C-70,U+72-75,U+77;
       }
     `}
   />
