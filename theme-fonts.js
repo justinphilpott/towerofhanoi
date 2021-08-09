04b7f634de6898753a0fc6b4cf59ef4ae59c6137
Justin Philpott <justin.philpott@protonmail.com>
2021-08-09 23:20:08 +0100

    Preload font, test

--- theme-fonts.js
+++ theme-fonts.js
@@ -9,7 +9,6 @@ const Fonts = () => (
         font-display: swap;
         font-weight: 400;
         src: url('./fonts/pattaya-regular-webfont.woff2') format('woff2'), url('./fonts/pattaya-regular-webfont.woff') format('woff');
-        unicode-range: U+21,U+2E,U+3F,U+47,U+48,U+4C,U+51,U+53,U+54,U+57,U+61,U+63-69,U+6C-70,U+72-75,U+77;
       }
     `}
   />
