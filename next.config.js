ae8c51269f1f26acf2b5dbda5891831d4e728fe1
Justin Philpott <justin.philpott@protonmail.com>
2021-08-13 19:57:11 +0100

    security headers

--- next.config.js
+++ next.config.js
@@ -9,13 +9,17 @@ const securityHeaders = [
     value: 'DENY'
   },
   {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block'
-  },
-  {
     key: 'X-Content-Type-Options',
     value: 'nosniff'
-  }
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
 ]
 
 module.exports = withBundleAnalyzer(withPWA({
